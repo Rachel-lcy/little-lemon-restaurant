fd56db67da31e91d33dcc7cb584459e36547b965
Rachel <hurui.lcy@gmail.com>
2024-11-02 18:43:55 -0600

    the first commit

---
+++ react-app/src/Header.jsx
@@ -0,0 +1,19 @@
+import logo from './assets/logo.jpg'
+function Header(){
+    return(
+      <header>
+        <img src={logo} alt="Little Lemon Logo" />
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Reservation</a></li>
+            <li><a href="#">Order Online</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+}
+export default Header;
---
+++ react-app/src/app.jsx
@@ -0,0 +1,14 @@
+import Header from  './Header.jsx'
+import './app.css'
+
+export function App() {
+  
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App;
