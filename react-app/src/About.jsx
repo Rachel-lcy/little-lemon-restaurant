43c9a7f8b8e125c0f056fc1cec1f4c4a6461f5db
Rachel <hurui.lcy@gmail.com>
2024-11-04 23:59:15 -0700

    update

--- react-app/src/About.jsx
+++ react-app/src/About.jsx
@@ -1,19 +1,22 @@
-import aboutPicture from './assets/about.jpg';
-import restaurantPicture from './assets/restaurant1.jpg'
+import aboutPicture from './assets/chef.jpg';
+import restaurantPicture from './assets/restaurant2.jpg'
 
 function About(){
   return(
-    <div>
+    <div className='all-about'>
       <div>
+      <div className='about-section'>
         <h2>Little Lemon</h2>
         <h3>Chicago</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. </p>
       </div>
-      <div>
-        <img src={aboutPicture} alt="" />
-        <img src={restaurantPicture} alt="" />
+      </div>
+      <div className='picture-section'>
+          <img src={restaurantPicture} alt="" className="picture"/>
+          <img src={aboutPicture} alt="" className="picture-overlapping"/>
       </div>
     </div>
+  
   );
 }
 
