bcedba30e8ddcaca25de43fd266d9db76ea50985
Rachel <hurui.lcy@gmail.com>
2024-11-05 19:26:11 -0700

    About done

--- react-app/src/About.jsx
+++ react-app/src/About.jsx
@@ -8,15 +8,20 @@ function About(){
       <div className='about-section'>
         <h2>Little Lemon</h2>
         <h3>Chicago</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. </p>
       </div>
       </div>
+
       <div className='picture-section'>
+        <div className='show-pictures'>
           <img src={restaurantPicture} alt="" className="picture"/>
           <img src={aboutPicture} alt="" className="picture-overlapping"/>
+        </div>  
       </div>
+      
     </div>
-  
+    
   );
 }
 
