cfaf2455f4716d4b74d4bfb3203c415505d61530
Rachel <hurui.lcy@gmail.com>
2024-11-06 12:24:38 -0700

    useState hook in booking page

--- react-app/src/BookingForm.jsx
+++ react-app/src/BookingForm.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './assets/logo.jpg';
 import {Link} from 'react-router-dom';
 import footerLogo from './assets/logo-footer.png';
 
 function BookingForm(){
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [guests, setGuests] = useState(1);
+  const [occasion, setOccasion] = useState('');
+
+  const [availableTimes, setAvailableTimes] = useState([
+    '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'
+  ])
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log({
+      date,
+      time,
+      guests,
+      occasion
+    });
+  }
+
   return(
     <>
       <div className="booking-header-container">
@@ -13,25 +32,48 @@ function BookingForm(){
         <h1 className='booking-header'>Little Lemon Restaurant Reservation</h1>
       </div>
 
-      <form className="booking-form" style={{ display: 'grid', maxWidth: '800px', gap: '20px' }}>
+      <form onSubmit = {handleSubmit} className="booking-form" style={{ display: 'grid', maxWidth: '800px', gap: '20px' }}>
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
+        <input 
+          type="date" 
+          id="res-date"
+          value= {date} 
+          onChange ={(e)=> setDate(e.target.value)}
+        />
 
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time">
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
+        <select 
+          id="res-time"
+          value = {time}
+          onChange = {(e) => setTime(e.target.value)}
+        >
+          {availableTimes.map((timeOption) => (
+            <option 
+              key={timeOption} 
+              value={timeOption}
+            >
+              {timeOption}
+            </option>
+          ))}
         </select>
 
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <input 
+          type="number" 
+          placeholder="1" 
+          min="1" 
+          max="10" 
+          id="guests" 
+          value={guests}
+          onChange = {(e)=> setGuests(e.target.value)}
+        />
 
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
+        <select 
+          id="occasion"
+          value={occasion}
+          onChange = {(e)=> setOccasion(e.target.value)}
+        >
           <option>Birthday</option>
           <option>Anniversary</option>
         </select>
