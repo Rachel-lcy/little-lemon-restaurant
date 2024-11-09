01a2e374845b109ed9e91c9548054f4be012ce7b
Rachel <hurui.lcy@gmail.com>
2024-11-08 17:37:13 -0700

    update

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -10,7 +10,7 @@ root.render(<App />);
 function initializeTimes() {
   const today = new Date();
   return fetchAPI(today);
-  //return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  
 }
 
 function updateTimes(state, action) {
